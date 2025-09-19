3b0fbd208546378799c2ec2846a0fb01ff8eef48
MTruong2000 <truongdev2000@gmail.com>
2025-09-19 14:19:48 +0700

    create  ui profile listdown

--- src/funcs/scrolltosection.ts
+++ src/funcs/scrolltosection.ts
@@ -2,7 +2,7 @@ const scrollToSection = (id: string) => {
   const section = document.getElementById(id);
   if (section) {
     const y = section.getBoundingClientRect().top + window.scrollY - 80;
-    //   window.scrollTo({ top: y, behavior: "smooth" });
+      // window.scrollTo({ top: y, behavior: "smooth" });
     window.scrollTo({ top: y });
   }
 };
